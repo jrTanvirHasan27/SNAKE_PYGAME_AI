eea4e42a431f5ac8463501fbe15c1254a9b8f6c6
jrTanvirHasan27 <jr.tanvirhasan@gmail.com>
2022-04-26 15:49:25 +0600

    Initializing Direction class, Point obj etc etc.
    Created Direction class, Point object, initialized game state in the Snakegame class and placed the food randomly.

--- snake_game.py
+++ snake_game.py
@@ -1,7 +1,20 @@
 import pygame
 import random
+from collections import namedtuple
 from enum import Enum
+
 pygame.init()
+
+
+class Direction(Enum):
+    RIGHT = 1
+    UP = 3
+    DOWN = 4
+    LEFT = 2
+
+
+Point = namedtuple('Point', ('x', 'y'))
+BLOCK_SIZE = 20
 
 
 class SnakeGame:
@@ -15,7 +28,23 @@ class SnakeGame:
         self.clock = pygame.time.Clock()
 
         # init game state
-        self.direction = "R"
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w / 2, self.h / 2)
+        self.snake = [self.head,
+                      Point(self.head.x - BLOCK_SIZE, self.head.y),
+                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+
+        self.score = 0
+        self.food = None
+        self._place_food()
+
+    def _place_food(self):
+        x = random.randint(0, (self.w-BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
 
     def play_step(self):
         pass
