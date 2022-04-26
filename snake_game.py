58d174920fb0f2669ef55e5e092333e766161765
jrTanvirHasan27 <jr.tanvirhasan@gmail.com>
2022-04-26 16:01:12 +0600

    Fixed a bug and initiate the game over part.

--- snake_game.py
+++ snake_game.py
@@ -13,7 +13,7 @@ class Direction(Enum):
     LEFT = 2
 
 
-Point = namedtuple('Point', ('x', 'y'))
+point = namedtuple('point', ('x', 'y'))
 BLOCK_SIZE = 20
 
 
@@ -23,17 +23,17 @@ class SnakeGame:
         self.w = w
         self.h = h
         # init display
-        self.display = pygame.display.set_mode(self.w, self.h)
+        self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
 
         # init game state
         self.direction = Direction.RIGHT
 
-        self.head = Point(self.w / 2, self.h / 2)
+        self.head = point(self.w / 2, self.h / 2)
         self.snake = [self.head,
-                      Point(self.head.x - BLOCK_SIZE, self.head.y),
-                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+                      point(self.head.x - BLOCK_SIZE, self.head.y),
+                      point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
 
         self.score = 0
         self.food = None
@@ -42,12 +42,18 @@ class SnakeGame:
     def _place_food(self):
         x = random.randint(0, (self.w-BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
         y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
-        self.food = Point(x, y)
+        self.food = point(x, y)
         if self.food in self.snake:
             self._place_food()
 
     def play_step(self):
-        pass
+        # Collect User Input
+        # Move
+        # Check if game over
+        # update Ui and clock
+        # return game over and score
+        game_Over = False
+        return game_Over, self.score
 
 
 if __name__ == '__main__':
@@ -55,8 +61,12 @@ if __name__ == '__main__':
 
     # game Loop
     while True:
-        game.play_step()
+        game_Over, score = game.play_step()
 
         # break if game over
+        if game_Over:
+            break
+
+    print('Final Score: ', score)
 
     pygame.quit()
