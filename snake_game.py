1269ca42c5fd1f08a2c576c8237fdc85339d3a87
jrTanvirHasan27 <jr.tanvirhasan@gmail.com>
2022-04-26 15:15:37 +0600

    Created Functionalities of the Snake class
    Snakes Step, Direction and Display properties/classes have been added.

--- snake_game.py
+++ snake_game.py
@@ -1,1 +1,33 @@
 import pygame
+import random
+from enum import Enum
+pygame.init()
+
+
+class SnakeGame:
+
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+        # init display
+        self.display = pygame.display.set_mode(self.w, self.h)
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+
+        # init game state
+        self.direction = "R"
+
+    def play_step(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = SnakeGame()
+
+    # game Loop
+    while True:
+        game.play_step()
+
+        # break if game over
+
+    pygame.quit()
